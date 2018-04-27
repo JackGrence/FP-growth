5f40aebe193496054782824823ff056b5f96d427
JackGrence <green1181772@gmail.com>
2018-04-27 17:39:11 +0800

    Compelete fp_growth function

--- main.c
+++ main.c
@@ -26,6 +26,8 @@ main (int argc, char *argv[])
   heap_ary *sorted_heap_ary;
   fp_tree *first_tree;
 
+  begin = clock();
+
   if (argc < 3)
     {
       printf ("Usage: ./fp-growth FILE MIN_SUP\n");
@@ -63,7 +65,12 @@ main (int argc, char *argv[])
 
   first_tree = get_fp_tree (f, id_to_order, sorted_heap_ary->ary_len);
   first_tree->order_to_ID = sorted_heap_ary;
+
+  item_num = 0;
   fp_growth (first_tree);
+
+  calc_time ("finish");
+  printf ("frq: %d\n", item_num);
 
   //print_fp_tree (first_tree);
 
@@ -175,7 +182,7 @@ get_fp_tree (FILE *f, table_list *id_to_order, int order_ary_len)
       if (insert_ary_len > 0)
 	{
 	  //print_ary (insert_ary, insert_ary_len);
-	  fp_tree_insert (insert_ary, insert_ary_len, tree);
+	  fp_tree_insert (insert_ary, insert_ary_len, tree, 1);
 	}
 
       if (t_ary != NULL && len != -1)
@@ -191,48 +198,131 @@ fp_growth (fp_tree *tree)
     return;
   int i;
   int ary_ind;
-  int *item_count_ary;
+  int *id_count_ary;
+  int *old_to_new_order;
+  int *item_ary;
+  int new_item_size;
   fp_node_link_list *cur_list;
   fp_node *cur_node;
+  fp_tree *new_tree;
   heap_ary *new_order;
   heap_node *tmp_node;
 
-  item_count_ary = (int *) alloca (sizeof (int) * tree->header_table_len);
+  new_item_size = tree->freq_item_set->size + 1;
+  item_ary = (int *) alloca (new_item_size * sizeof (int));
+  id_count_ary = (int *) alloca (sizeof (int) * tree->header_table_len);
+  old_to_new_order = (int *) alloca (sizeof (int) * tree->header_table_len);
+  tmp_node = (heap_node *) alloca (sizeof (heap_node));
   // bottom up header table of FP tree
   for (i = tree->header_table_len - 1; i >= 0; i--)
     {
       if (tree->header_table[i]->next == NULL)
 	continue;
+
       // calculate new freq
       // scan header table's list
-      memset (item_count_ary, 0, sizeof (int) * tree->header_table_len);
+      memset (id_count_ary, 0, sizeof (int) * tree->header_table_len);
       cur_list = tree->header_table[i]->next;
       while (cur_list != NULL)
 	{
 	  //scan node and count
 	  cur_node = cur_list->node;
-	  while (cur_node != NULL)
+	  while (cur_node != tree->root)
 	    {
-	      item_count_ary[cur_node->index] += cur_list->node->count;
+	      id_count_ary[cur_node->index] += cur_list->node->count;
 	      cur_node = cur_node->parent;
 	    }
 	  cur_list = cur_list->next;
 	}
-      // get item_count_ary, index is this tree's freq order, value is new freq
+      // id_count_ary, index is this tree's freq order, value is new freq
       // sort new freq and old order pair
-      new_order = create_heap_ary (tree->header_table_len - 1);
-      tmp_node = (heap_node *) alloca (sizeof (heap_node));
-      for (ary_ind = 0; ary_ind < tree->header_table_len - 1; ary_ind++)
+      cur_node = tree->header_table[i]->next->node;
+      new_order = create_heap_ary (i);
+      for (ary_ind = 0; ary_ind < i; ary_ind++)
 	{
-	  if (item_count_ary[ary_ind] < MIN_SUP)
+	  if (id_count_ary[ary_ind] < MIN_SUP || ary_ind == cur_node->index)
 	    continue;
+	  //tmp_node->itemID = tree->order_to_ID->ary[ary_ind + 1].itemID;
 	  tmp_node->itemID = ary_ind;
-	  tmp_node->frequent = item_count_ary[ary_ind];
+	  tmp_node->frequent = id_count_ary[ary_ind];
 	  insert_min_heap (new_order, tmp_node);
 	}
+
+      // record item: frq
+      //print_ary (tree->freq_item_set->items, tree->freq_item_set->size);
+      //printf ("[%d]", tree->order_to_ID->ary[cur_node->index + 1].itemID);
+      //printf ("freq: %d\n", id_count_ary[cur_node->index]);
+      item_num++;
+
       new_order->ary_len = sort_min_heap (new_order);
-      printf ("----\n");
-      print_min_heap (new_order);
-      printf ("----\n");
+      if (new_order->ary_len == 0)
+	{
+	  // no frequent item >= MIN_SUP
+	  free_heap_ary (new_order);
+	  continue;
+	}
+
+      // get old_order to new_order array
+      heap_node *tmp;
+      memset (old_to_new_order, -1, sizeof (int) * tree->header_table_len);
+      for (ary_ind = 0; ary_ind < new_order->ary_len; ary_ind++)
+	{
+	  tmp = &new_order->ary[ary_ind + 1];
+	  old_to_new_order[tmp->itemID] = ary_ind;
+	  // parse new_order->ary's itemID to real itemID
+	  tmp->itemID = tree->order_to_ID->ary[tmp->itemID + 1].itemID;
+	}
+
+      // generate new tree
+      // get new tree's frequent item set
+      memcpy (item_ary, tree->freq_item_set->items, (new_item_size - 1) * sizeof (int));
+      item_ary[new_item_size - 1] = tree->order_to_ID->ary[cur_node->index + 1].itemID;
+
+      new_tree = create_fp_tree (create_item_set (item_ary, new_item_size),
+				 new_order->ary_len);
+      new_tree->order_to_ID = new_order;
+
+      // insert to new tree
+      int *transaction_ary;
+      int transaction_len;
+      int count;
+      transaction_ary = id_count_ary;
+      cur_list = tree->header_table[i]->next;
+      while (cur_list != NULL)
+	{
+	  // get trasaction
+	  memset (transaction_ary, 0, i * sizeof (int));
+	  cur_node = cur_list->node;
+	  count = cur_node->count;
+	  cur_node = cur_node->parent;
+	  while (cur_node != tree->root)
+	    {
+	      ary_ind = old_to_new_order[cur_node->index];
+	      if (ary_ind != -1)
+		{
+		  // record used item
+		  transaction_ary[ary_ind] = 1;
+		}
+	      cur_node = cur_node->parent;
+	    }
+	  transaction_len = 0;
+	  for (ary_ind = 0; ary_ind < i; ary_ind++)
+	    {
+	      // set transaction array
+	      if (transaction_ary[ary_ind])
+		{
+		  transaction_ary[transaction_len] = ary_ind;
+		  transaction_len++;
+		}
+	    }
+	  if (transaction_len)
+	    {
+	      fp_tree_insert (transaction_ary, transaction_len,
+			      new_tree, count);
+	    }
+	  cur_list = cur_list->next;
+	}
+      fp_growth (new_tree);
+      free_fp_tree (new_tree);
     }
 }
